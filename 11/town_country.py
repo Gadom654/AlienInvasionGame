542964eceb6de81ae02c8fd2c2646365e66e8b3d
Gadom <69717399+Gadom654@users.noreply.github.com>
2021-05-28 17:14:58 +0200

    Delete 11 directory

--- 11/town_country.py
+++
@@ -1,7 +0,0 @@
-def town_country(town,country,population=""):
-	if population:
-		town_name =	town.title() + ", " + country.title() + \
-		 " - population " + population
-	else:	
-		town_name = town.title() + ", " + country.title()
-	return town_name
