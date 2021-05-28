d07d6b63336e134346ab7d9e4a654ac95ab65034
Gadom <69717399+Gadom654@users.noreply.github.com>
2021-05-28 17:15:09 +0200

    Delete 3 directory

--- 3/4.py
+++
@@ -1,8 +0,0 @@
-#Tworzenie listy gości
-ppl = ["Karina","Katol","Damian"]
-msg1= ppl[0] + " zapraszam cię na obiad."
-msg2= ppl[1] + " zapraszam cię na obiad."
-msg3= ppl[2] + " zapraszam cię na obiad."
-print(msg1)
-print(msg2)
-print(msg3)
